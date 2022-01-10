0c57e981d5e05e26b0f031abc1417c40c54d07af
COMoER <zhengyu0730@sjtu.edu.cn>
2022-01-10 15:56:02 +0800

    add train log

--- project/model/algo.py
+++ project/model/algo.py
@@ -5,9 +5,9 @@ import numpy as np
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
 import torch.nn.functional as F
+
 HIDDEN_SIZE = 256
 from tqdm import tqdm
-
 
 import random
 
@@ -19,24 +19,46 @@ def setup_seed(seed):
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
-    torch.backends.cudnn.deterministic = True
+
 
 setup_seed(2021)
 
+
 class RandomSelect(nn.Module):
-    def __init__(self,length,T):
+    def __init__(self, length, T, head,test):
         super(RandomSelect, self).__init__()
         self.length = length
         self.T = T
-    def forward(self,paths):
+        self.head = head
+        self.test = test
+        if test:
+            # test dataset not random select length while different path
+            if self.head:
+                self.l = random.randint(round(length * 0.3), round(length * 0.7))
+                self.start = 0
+            else:
+                self.l = random.randint(round(length * 0.5), round(length * 0.9))
+                self.start = random.randint(0, length - self.l + 1)
+
+    def forward(self, paths):
         length = self.length
-        l = random.randint(round(length*0.5),round(length*0.9))
-        start = random.randint(0,length-l+1)
-        mask = torch.zeros((self.T,),dtype=torch.long)
-        mask[start:start+l] = 1
-        return paths*mask
+        if self.test:
+            mask = torch.zeros((self.T,), dtype=torch.long)
+            mask[self.start:self.start + self.l] = 1
+            return paths * mask
+        if self.head:
+            l = random.randint(round(length * 0.3), round(length * 0.7))
+            start = 0
+        else:
+            l = random.randint(round(length * 0.5), round(length * 0.9))
+            start = random.randint(0, length - l + 1)
+        mask = torch.zeros((self.T,), dtype=torch.long)
+        mask[start:start + l] = 1
+        return paths * mask
+
+
 class TrajectoryDataset(Dataset):
-    def __init__(self, sample, y, vocal_max):
+    def __init__(self, sample, y, vocal_max,test=False,head=False,random=False):
         """
 
         Args:
@@ -48,36 +70,42 @@ class TrajectoryDataset(Dataset):
         super(TrajectoryDataset, self).__init__()
         self.vocal_max = vocal_max
         self.T = max([len(v) for v in sample])
+        self.random = random
         self.sample = []
         self.randomSelect = []
         for sen in tqdm(sample):
             senpad = np.zeros(self.T, np.long)
-            self.randomSelect.append(RandomSelect(len(sen),self.T))
-            senpad[:len(sen)] = np.array(sen) + 1 # shift one position to contain padding 0
+            if self.random:
+                self.randomSelect.append(RandomSelect(len(sen), self.T,head,test))
+            senpad[:len(sen)] = np.array(sen) + 1  # shift one position to contain padding 0
             self.sample.append(senpad)
         self.arrival = y
-        self.paths = torch.LongTensor(self.sample) # (N,T)
-        self.nodes = torch.FloatTensor(self.arrival).view(-1,2)  # (N,2)
+        self.paths = torch.LongTensor(self.sample)  # (N,T)
+        self.nodes = torch.FloatTensor(self.arrival).view(-1, 2)  # (N,2)
 
     def __len__(self):
         return len(self.sample)
 
     def __getitem__(self, idx):
-        return self.randomSelect[idx](self.paths[idx]),self.arrival[idx]
+        if self.random:
+            return self.randomSelect[idx](self.paths[idx]), self.arrival[idx]
+        else:
+            return self.paths[idx], self.arrival[idx]
+
+
 class mlp(nn.Module):
-    def __init__(self,vocal_max):
+    def __init__(self, vocal_max):
         super(mlp, self).__init__()
-        self.embed = nn.Embedding(vocal_max+1,32,padding_idx=0) # padding
-        self.conv = nn.Conv1d(32,32,kernel_size=3,padding=1)
-        self.fc1 = nn.Linear(32,HIDDEN_SIZE)
-        self.fc2 = nn.Linear(HIDDEN_SIZE,2)
+        self.embed = nn.Embedding(vocal_max + 1, 32, padding_idx=0)  # padding
+        self.conv = nn.Conv1d(32, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32, HIDDEN_SIZE)
+        self.fc2 = nn.Linear(HIDDEN_SIZE, 2)
         self.dropout = nn.Dropout(0.5)
-    def forward(self,x):
-        embedding = self.embed(x) # B,T,32
-        embedding = embedding.permute(0,2,1) # B,C,T
+
+    def forward(self, x):
+        embedding = self.embed(x)  # B,T,32
+        embedding = embedding.permute(0, 2, 1)  # B,C,T
         l1 = F.relu(self.conv(embedding))
-        l1_max = torch.max(l1,dim=-1)[0] # B,32 over-time-maxpooling
+        l1_max = torch.max(l1, dim=-1)[0]  # B,32 over-time-maxpooling
         l2 = self.dropout(F.relu(self.fc1(l1_max)))
         return self.fc2(l2)
-
-
--- project/script/train.py
+++ project/script/train.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import sys
+import os
+import logging
+import argparse
+from datetime import datetime
+import yaml
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+install_path = str(Path(__file__).resolve().parent.parent)
 
 import torch
 import torch.nn as nn
@@ -10,6 +16,7 @@ import numpy as np
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
+
 
 from model.algo import mlp,device,TrajectoryDataset
 from utils.common import loadGeoHashEncoder,LonLatVocal
@@ -21,19 +28,41 @@ N = len(modelGeo) + 1
 del modelGeo
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--random_length",action="store_true",help="using random pick training")
+    parser.add_argument("--head",action="store_true",help="using random prefix")
+    parser.add_argument("--meta",action="store_true",help="add metadata to train")
+    parser.add_argument("--out",action="store_true",help="add train data that has outlier geohash location")
+    parser.add_argument("--lr",type=float,default=0.01,help="learning rate")
+    parser.add_argument("--size",type=int,default=100000,help="# of randomly picked from trainset")
+    parser.add_argument("--epoch",type=int,default=100,help="# of max epoch")
+    args = parser.parse_args()
+
+    dir = os.path.join(install_path,"log",datetime.now().strftime("%Y%m%d_%H%M%S"))
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    with open(os.path.join(dir,"args.yaml"),'w') as f:
+        ym = yaml.dump(vars(args),f)
+    log_fname = os.path.join(dir, 'log_train.txt')
+    LOGGING_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    DATE_FORMAT = '%Y%m%d %H:%M:%S'
+    logging.basicConfig(level=logging.DEBUG, format=LOGGING_FORMAT, datefmt=DATE_FORMAT, filename=log_fname)
+    logger = logging.getLogger("Trainer")
+
     model = mlp(N).to(device)
-    optim = torch.optim.Adam(model.parameters(),0.01)
+    optim = torch.optim.Adam(model.parameters(),args.lr)
     critic = torch.nn.MSELoss()
     print("[INFO] load dataset")
     # df_train = pd.read_csv("../../dataset/train.csv",delimiter=',')
     # print("[INFO] doing preprocessing")
-    # X,y = pipeline(df_train,100000,verbose=True)
+    # X,y = pipeline(df_train,args.size,verbose=True)
     with open("../../train_data.pth",'rb') as f:
         X,y = pkl.load(f)
     size = len(X)
     print("[INFO] doing preprocessing finished")
-    train_dataloader = DataLoader(TrajectoryDataset(X[:round(size*0.95)],y[:round(size*0.95)],56960),200,True,drop_last=True)
-    test_data = TrajectoryDataset(X[round(size*0.95):],y[round(size*0.95):],56960)
+    train_dataloader = DataLoader(TrajectoryDataset(X[:round(size*0.95)],y[:round(size*0.95)],56960,test=False,head=args.head,random=args.random_length),200,True,drop_last=True)
+    test_data = TrajectoryDataset(X[round(size*0.95):],y[round(size*0.95):],56960,test=True,head=args.head,random=args.random_length)
     test_dataloader = DataLoader(test_data,len(test_data))
     print("[INFO] data load finish")
     X_test,y_test = None,None
@@ -41,22 +70,29 @@ if __name__ == '__main__':
         X_test = x.to(device)
         y_test = y.to(device)
     error_best = 100
-    for epoch in range(100):
+    for epoch in range(args.epoch):
+        logger.info('**** EPOCH %03d ****' % (epoch))
         acc = 0
         n = 0
         tbar = tqdm(train_dataloader)
         model.train()
+        loss_sum = 0
         for X,y in tbar:
             pred = model(X.to(device))
             loss = critic(pred,y.to(device).float())
             optim.zero_grad()
             loss.backward()
             optim.step()
+            loss_sum += loss.cpu().detach().item()
             tbar.set_postfix({'loss':"%.3f"%loss.cpu().detach().item()})
+
         model.eval()
         pred = model(X_test).detach()
         err = critic(pred,y_test).cpu().detach().item()
         print(f"epoch {epoch} mse is {err:.3f}")
+        logger.info('train_loss: %.6f',loss_sum/len(tbar))
+        logger.info('eval_loss: %.6f',err)
         if err<error_best:
             error_best = err
-            torch.save(model.state_dict(),"model_best.pt")
+            print(f"[BEST] epoch {epoch} mse is {err:.3f}")
+            torch.save(model.state_dict(),os.path.join(dir,"model_best.pt"))
