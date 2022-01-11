cd8588260fb5efa5589453383cf62801188e0143
COMoER <zhengyu0730@sjtu.edu.cn>
2022-01-11 18:43:32 +0800

    add meta

--- project/script/train.py
+++ project/script/train.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import yaml
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 install_path = str(Path(__file__).resolve().parent.parent)
+dataset_path = str(Path(__file__).resolve().parent.parent.parent)
 
 import torch
 import torch.nn as nn
@@ -18,12 +19,13 @@ from torch.utils.data.dataloader import DataLoader
 from tqdm import tqdm
 
 
-from model.algo_first_point import mlp,device,TrajectoryDataset
+from model.algo_first_point import mlpMetaEmbedding,device,TrajectoryDataset
 from utils.common import loadGeoHashEncoder,LonLatVocal,setup_seed
 from utils.coordination import LatLonTransform
 from utils.preprocess import pipeline
+from utils.meta import getEmbedInfo
 
-modelGeo:LonLatVocal = loadGeoHashEncoder("pretrained/geohashmodel.pt")
+modelGeo:LonLatVocal = loadGeoHashEncoder(os.path.join(install_path,"pretrained","geohashmodel.pt"))
 N = len(modelGeo) + 1
 del modelGeo
 
@@ -32,6 +34,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--random_length",action="store_true",default=True,help="using random pick training")
     parser.add_argument("--head",action="store_true",help="using random prefix")
+    parser.add_argument("--prefix",action="store_true",help="using 5 prefix input")
     parser.add_argument("--meta",action="store_true",help="add metadata to train")
     parser.add_argument("--out",action="store_true",help="add train data that has outlier geohash location")
     parser.add_argument("--lr",type=float,default=0.01,help="learning rate")
@@ -50,28 +53,46 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format=LOGGING_FORMAT, datefmt=DATE_FORMAT, filename=log_fname)
     logger = logging.getLogger("Trainer")
 
-    model = mlp(N).to(device)
+    metainfo = getEmbedInfo() if args.meta else None
+    model = mlpMetaEmbedding(N,meta=args.meta, first_point=args.prefix,embed_table=metainfo).to(device)
     optim = torch.optim.Adam(model.parameters(),args.lr)
     critic = torch.nn.MSELoss()
     print("[INFO] load dataset")
-    df_train = pd.read_csv("dataset/train.csv",delimiter=',')
+    df_train = pd.read_csv(os.path.join(dataset_path,"dataset","train.csv"),delimiter=',')
     print("[INFO] doing preprocessing")
-    X,y,first_points = pipeline(df_train,args.size,verbose=True)
+    X,y,first_points,meta_data = pipeline(df_train,args.size,verbose=True)
 
     #with open("../../train_data.pth",'rb') as f:
         #X,y = pkl.load(f)
 
     size = len(X)
     print("[INFO] doing preprocessing finished")
-    train_dataloader = DataLoader(TrajectoryDataset(X[:round(size*0.95)],y[:round(size*0.95)],first_points[:round(size*0.95)],56960,test=False,head=args.head,random=args.random_length),300,True,drop_last=True)
-    test_data = TrajectoryDataset(X[round(size*0.95):],y[round(size*0.95):],first_points[round(size*0.95):],56960,test=True,head=args.head,random=args.random_length)
+    # sample, y,vocal_max,meta_data=None,train_first_points=None,
+    # test=False,head=False,random=False,prefix=False,meta=False
+    train_dataloader = DataLoader(TrajectoryDataset(X[:round(size*0.95)],y[:round(size*0.95)],
+                                                    56960,
+                                                    meta_data=meta_data[:round(size*0.95)] if args.meta else None,
+                                                    train_first_points = first_points[:round(size*0.95)] if args.prefix else None,
+                                                    test=False,
+                                                    head=args.head,
+                                                    random=args.random_length,prefix=args.prefix,
+                                                    meta=args.meta),300,True,drop_last=True)
+    test_data = TrajectoryDataset(X[round(size*0.95):],y[round(size*0.95):],
+                                                    56960,
+                                                    meta_data=meta_data[round(size*0.95):] if args.meta else None,
+                                                    train_first_points = first_points[round(size*0.95):] if args.prefix else None,
+                                                    test=True,
+                                                    head=args.head,
+                                                    random=args.random_length,prefix=args.prefix,
+                                                    meta=args.meta)
     test_dataloader = DataLoader(test_data,len(test_data))
     print("[INFO] data load finish")
-    X_test,y_test,first_points_test = None,None,None
-    for x,y,first_points in test_dataloader:
+    # X_test,y_test,first_points_test,meta_data_test = None,None,None,None
+    for x,y,first_points,meta_data in test_dataloader:
         X_test = x.to(device)
         y_test = y.to(device)
         first_points_test = first_points.to(device)
+        meta_data_test = meta_data.to(device)
     error_best = 100
     setup_seed(2021)
     for epoch in range(args.epoch):
@@ -81,8 +102,12 @@ if __name__ == '__main__':
         tbar = tqdm(train_dataloader)
         model.train()
         loss_sum = 0
-        for X,y,first_points in tbar:
-            pred = model(X.to(device),first_points.to(device))
+        for X,y,first_points,meta_data in tbar:
+            if first_points is not None:
+                first_points = first_points.to(device)
+            if meta_data is not None:
+                meta_data = meta_data.to(device)
+            pred = model(X.to(device),first_points,meta_data)
             loss = critic(pred,y.to(device).float())
             optim.zero_grad()
             loss.backward()
@@ -91,7 +116,7 @@ if __name__ == '__main__':
             tbar.set_postfix({'loss':"%.3f"%loss.cpu().detach().item()})
 
         model.eval()
-        pred = model(X_test,first_points_test).detach()
+        pred = model(X_test,first_points_test,meta_data_test).detach()
         err = critic(pred,y_test).cpu().detach().item()
         print(f"epoch {epoch} mse is {err:.3f}")
         logger.info('train_loss: %.6f',loss_sum/len(tbar))
--- project/utils/preprocess.py
+++ project/utils/preprocess.py
@@ -1,25 +1,32 @@
 from pathlib import Path
 import sys
+import os
 sys.path.append(Path(__file__).resolve().parent.parent)
+install_path = str(Path(__file__).resolve().parent.parent)
 
 import pandas as pd
 import numpy as np
 import pickle as pkl
 from utils.common import str2trajectory, loadGeoHashEncoder, LonLatVocal,setup_seed
 from utils.coordination import loadTransform, LatLonTransform
+from utils.meta import mapMeta
 
 
 def pipeline(df, n=100000,test=False,verbose=True):
 
-    modelGeo: LonLatVocal = loadGeoHashEncoder("pretrained/geohashmodel.pt")
+    modelGeo: LonLatVocal = loadGeoHashEncoder(os.path.join(install_path,"pretrained","geohashmodel.pt"))
 
     if test:
+        df,meta_index = mapMeta(df)
+        meta_data = []
+        for meta in meta_index:
+            meta_data.append(df[meta].tolist())
         se, mask = str2trajectory(df, return_mask=True)
         seq, mask_seq = modelGeo.transform(se, arrival=False, return_mask=True, test=True)
         X = seq
         train_first_points = []
-        n_first_point = 5 #select first 5 point
-        trans:LatLonTransform = loadTransform("pretrained/trans.pt")
+        n_first_point = 5 # select first 5 point
+        trans:LatLonTransform = loadTransform(os.path.join(install_path,"pretrained","trans.pt"))
         for m, seq in zip(mask, se):
             if m:
                 lon = seq[::2]
@@ -33,9 +40,7 @@ def pipeline(df, n=100000,test=False,verbose=True):
                 position_xy=trans.transform(position)
                 train_first_points.append(position_xy)
 
-
-
-        return X,train_first_points
+            return X,train_first_points,np.array(meta_data).T
     setup_seed(2021)
     # from dataset sample n samples
     sample_df = df.sample(n)
@@ -52,8 +57,6 @@ def pipeline(df, n=100000,test=False,verbose=True):
     if verbose:
         print('.',end='')
 
-    arrival = np.array([seq[-1] for seq in whole_seq])
-    starter = np.array([seq[0] for seq in whole_seq])
     train_seq = np.array([seq[:-1] for seq in whole_seq])
     train_latlon_seq = []
     # get the arrival in lat-lon coordination
@@ -62,7 +65,7 @@ def pipeline(df, n=100000,test=False,verbose=True):
             train_latlon_seq.append((seq[-1], seq[-2]))
     train_first_points = []
     n_first_point = 5 #select first 5 point
-    trans:LatLonTransform = loadTransform("pretrained/trans.pt")
+    trans:LatLonTransform = loadTransform(os.path.join(install_path,"pretrained","trans.pt"))
     for m, seq in zip(mask, series):
         if m:
             seq = seq[:-2]
@@ -81,16 +84,16 @@ def pipeline(df, n=100000,test=False,verbose=True):
         print('.',end='')
 
     sample_df = sample_df[mask]
-    sample_df["arr"] = train_latlon_seq
-    sample_df["arrival"] = arrival
-    sample_df["starter"] = starter
-    sample_df["seq"] = train_seq
+    sample_df,meta_index = mapMeta(sample_df)
+    meta_data = []
+    for meta in meta_index:
+        meta_data.append(sample_df[meta].tolist())
 
-    X = sample_df['seq'].to_list()
-    y = np.array(sample_df['arr'].to_list())
+    X = train_seq
+    y = np.array(train_latlon_seq)
 
     # transform from lat-lon coordination to x-y coordination
     y = trans.transform(y)
     if verbose:
         print()
-    return X, y ,train_first_points
+    return X, y ,train_first_points,np.array(meta_data).T
